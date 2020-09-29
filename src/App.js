be8b99241aeedb98c7b4a4764c7b556f7913bfe4
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-29 16:40:03 -0400

    js done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class ImageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: '',
+      caption: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const imageInfo = this.state;
+    this.props.addImage(imageInfo);
+    // console.log(imageInfo);
+  }
+
+  handleInput(event) {
+    this.setState({[event.target.name]: event.target.value});
+    // console.log(event.target.value);
+  }
+
+  render() {
+
+    return (
+      <form className="col" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="image">Enter image URL.</label>
+          <input type="url" className="form-control" id="image" name="image" value={this.state.image} onChange={this.handleInput}/>
+          <label htmlFor="caption">Enter caption.</label>
+          <input type="text" className="form-control" id="caption" name="caption" value={this.state.caption} onChange={this.handleInput}/>
+        </div>
+        <button className="btn btn-primary">Add Student</button>
+    </form>
+    )
+  }
 }
 
-export default App;
+
+class ImageList extends React.Component{
+  render() {
+    console.log('this is a test ', this.props.images);
+    const images = this.props.images.map((image, index) => <li className='col-4' key={index}><img src={image.image} alt="#"/><p>{image.caption}</p></li>)
+    return(
+      <ul className="row no-gutters">{images}</ul>
+    )
+  }
+};
+
+
+
+class ImageBoard extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      images: [],
+    }
+    this.addImage = this.addImage.bind(this);
+  }
+
+  addImage(image) {
+    console.log('image', image);
+    const images = [...this.state.images];
+    images.push(image);
+    this.setState({images});
+    console.log(this.state.images);
+  }
+
+  render() {
+    return(
+      <div className='container'>
+        <ImageForm addImage={this.addImage}/>
+        <ImageList images = {this.state.images}/>
+      </div>
+  )
+  }
+}
+
+export default ImageBoard;
